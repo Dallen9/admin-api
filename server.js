fd8794ba65330f072d7ff0596389256896ebab24
Donovan <allendonovan@gmail.com>
2020-10-28 11:49:22 -0700

    added mLab

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-const { options } = require('./routes/auth');
 require('dotenv').config()
 
 const app = express();
