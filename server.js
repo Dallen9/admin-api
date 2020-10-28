dcd2dfcd5187017a3b34a18853493f6911c101b5
Donovan <allendonovan@gmail.com>
2020-10-28 12:31:10 -0700

    added new env var

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 
 app.use(cors())
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
 
 //Define routes
 app.use('/api/users', require('./routes/users'));
